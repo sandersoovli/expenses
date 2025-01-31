cb49411289a0e26a5b9f2bb9409f626ca944716f
SanderSoovli <sander.soovali@voco.ee>
2025-01-31 12:59:33 +0200

    3.Ulesanne_31.01.25

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,18 +1,19 @@
 import './ExpenseItem.css'
 import '../App.jsx'
 import  ExpenseDate  from './ExpenseDate.jsx'
+import Card from './Card.jsx'
 
 const ExpenseItem = (props) => {
 
 
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate date={props.data.date}/>
             <div className='expense-item_description'>
                 <h2>{props.data.title}</h2>
                 <div className='expense-item_price'>{props.data.price}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
